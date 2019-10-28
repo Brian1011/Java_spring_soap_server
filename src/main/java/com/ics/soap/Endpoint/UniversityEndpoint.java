9772b9fbd8884cc8082adef171155e92717152b3
Brian1011 <mutindabrian26@gmail.com>
2019-10-28 12:39:22 +0300

    fix get university by name error

--- src/main/java/com/ics/soap/Endpoint/UniversityEndpoint.java
+++ src/main/java/com/ics/soap/Endpoint/UniversityEndpoint.java
@@ -12,10 +12,7 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class UniversityEndpoint {
     private static final String NAMESPACE_URI = "http://localhost:7991/universities";
-
-
     private UniversityRepository universityRepository;
-
 
     @Autowired
     public UniversityEndpoint(UniversityRepository universityRepository) {
